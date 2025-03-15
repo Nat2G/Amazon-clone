d0f14ca7b69ae5857f56778965753cfbcd821a15
NatinaelGebeyehu <natinaelgebeyehu2127@gmail.com>
2025-03-15 15:32:34 +0300

    Day 4

--- amazon/src/Components/Product/ProductCard.jsx
+++ amazon/src/Components/Product/ProductCard.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 import classes from "./Product.module.css"
+// import Product from './Product'
+import Link from 'react-router-dom';
 
-function ProductCard() {
+
+function ProductCard({prouct}) {
+    const {image, title, id, rating, price}=product;
   return (
-    <div>ProductCard</div>
+    <div className={'${classes.card__container}'}>
+        <Link to={}>
+            <img src={image} alt="" className={classes.img__container} />
+
+        </Link>
+        <div>
+            <h3>{title}</h3>
+            <div className={classes.rating}>
+
+                {/* {rating} */}
+                <Rating value={rating.rate} precision={0.1}/>
+                {/* {count} */}
+                <small>{rating.count}</small>
+
+            </div>
+            <div>
+                {/* {price} */}
+                <CurrencyFormat amount={price}/>
+            </div>
+            <button className={classes.button}>
+                add to cart
+            </button>
+        </div>
+        
+    </div>
   )
 }
 
